8eba717cbae699545d9bb81655fafdb9a3e7e395
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-12-21 14:35:19 -0700

    updates

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,10 +5,10 @@ const newFormHandler = async (event) => {
     const needed_funding = document.querySelector('#project-funding').value.trim();
     const description = document.querySelector('#project-desc').value.trim();
   
-    if (name && needed_funding && description) {
+    if (name && description) {
       const response = await fetch(`/api/projects`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ name, description }),
         headers: {
           'Content-Type': 'application/json',
         },
